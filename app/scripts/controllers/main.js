2f6355db534b2b80a588ef9e2f4e82aced59e326
mariozero <mxriozero@gmail.com>
2015-08-26 10:11:59 -0400

    version 0.0.2
    los primeros paso con Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name serviciosProTecApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the serviciosProTecApp
+ */
+angular.module('serviciosProTecApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
